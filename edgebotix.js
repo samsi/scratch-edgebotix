2067d9b9c2d45017f86a1275c32377ca6dbd42fd
Samuel Simon <samuel31@gmail.com>
2016-02-28 21:11:18 +0800

    Add command descriptions

--- edgebotix.js
+++ edgebotix.js
@@ -1,3 +1,5 @@
+var botix_listener = '127.0.0.1';
+
 (function(ext) {
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
@@ -16,7 +18,62 @@
 
     };
 
-    ext.block_led = function(state) {
+    ext.block_led = function(led_state) {
+        $.ajax({
+            url: botix_listener,
+            dataType: 'jsonp',
+            success: function( weather_data ) {
+                temperature = weather_data['main']['temp'];
+                callback(temperature);
+            }
+        });
+    };
+
+    ext.block_wheels = function(left_wheel, right_wheel) {
+
+    };
+
+    ext.block_wheels_calibrate = function(left_wheel, right_wheel) {
+
+    };
+
+    ext.block_buzzer = function(duration, freq) {
+
+    };
+
+    ext.block_imperial_march = function() {
+
+    };
+
+    ext.block_halt = function() {
+
+    };
+
+    ext.block_get_temperature = function(callback) {
+
+    };
+
+    ext.block_get_light = function(callback) {
+
+    };
+
+    ext.block_get_obstacle = function(callback) {
+
+    };
+
+    ext.block_get_sonars = function(callback) {
+
+    };
+
+    ext.block_get_odometry = function(callback) {
+
+    };
+
+    ext.block_get_power = function(callback) {
+
+    };
+
+    ext.block_get_acceleration = function(callback) {
 
     };
 
@@ -24,10 +81,28 @@
     var descriptor = {
         blocks: [
             // Block type, block name, function name
-            [' ', 'Connect', 'block_connect'],
-            [' ', 'Disconnect', 'block_disconnect'],
+            ['', 'Connect', 'block_connect'],
+            ['', 'Disconnect', 'block_disconnect'],
 
-            [' ', 'Turn LED %m.stateOnOff', 'block_led', 'on'],
+            // Output 
+            ['', 'Turn LED %m.stateOnOff', 'block_led', 'on'],
+            //[' ', 'Turn LED on', 'block_led', 'on'],
+            //[' ', 'Turn LED off', 'block_led', 'on'],
+
+            ['', 'wheels(%n,%n)', 'block_wheels', 1.00, 1.00],
+            ['', 'wheel_calibrate(%n, %n)', 'block_wheels_calibrate', 1.00, 1.00],
+            ['', 'buzzer %n %n', 'block_buzzer', 1000, 500],
+            ['', 'Imperial march', 'block_imperial_march'],
+            ['', 'Halt', 'block_halt'],
+
+            // Input
+            ['', 'Get temperature', 'block_get_temperature'],
+            ['', 'Get light', 'block_get_light'],
+            ['', 'Get obstacle', 'block_get_obstacle'],
+            ['', 'Get sonars', 'block_get_sonars'],
+            ['', 'Get odometry', 'block_get_odometry'],
+            ['', 'Get power', 'block_get_power'],
+            ['', 'Get acceleration', 'block_get_acceleration'],
         ],
         menus: {
             stateOnOff: ['on', 'off']
