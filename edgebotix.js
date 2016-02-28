5593666c2d7749d717bdf78790c53d2b298b4155
Samuel Simon <samuel31@gmail.com>
2016-02-28 21:16:51 +0800

    Add comma to description

--- edgebotix.js
+++ edgebotix.js
@@ -79,8 +79,8 @@
             //[' ', 'Turn LED on', 'block_led', 'on'],
             //[' ', 'Turn LED off', 'block_led', 'on'],
 
-            ['', 'wheels left ( left: %n, right: %n )', 'block_wheels', 1.00, 1.00],
-            ['', 'wheel_calibrate ( left: %n, right: %n )', 'block_wheels_calibrate', 1.00, 1.00],
+            ['', 'wheels (left: %n , right: %n )', 'block_wheels', 1.00, 1.00],
+            ['', 'wheel_calibrate(left: %n , right: %n )', 'block_wheels_calibrate', 1.00, 1.00],
             ['', 'buzzer %n %n', 'block_buzzer', 1000, 500],
             ['', 'Imperial march', 'block_imperial_march'],
             ['', 'Halt', 'block_halt'],
